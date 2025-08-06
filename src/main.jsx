e60eb4ae88ae07427e539b9c97d405fd20c15039
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-06 19:20:17 +0530

    i18 package in installed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./i18n/i18n.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <React.Suspense fallback="loading...">
+      <App />
+    </React.Suspense>
+  </React.StrictMode>
+);
